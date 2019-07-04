4ecbbbc8725a9283416553a90af931b70f8e738f
k.struzhkin <k.struzhkin@rbkadmins-MacBook-Pro.local>
2019-07-04 10:16:27 +0300

    Add service prefix for consumer

---
+++ src/main/java/com/rbkmoney/fraudbusters/service/ConsumerGroupIdService.java
@@ -0,0 +1,28 @@
+package com.rbkmoney.fraudbusters.service;
+
+import com.rbkmoney.fraudbusters.util.KeyGenerator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ConsumerGroupIdService {
+
+    public static final String DELIMETER = "-";
+    @Value("${kafka.consumer.prefix}")
+    private String prefix;
+
+    public String generateRandomGroupId(String group) {
+        String groupId = KeyGenerator.generateKey(generateGroupId(group) + DELIMETER);
+        log.info("generateRandomGroupId groupId: {}", groupId);
+        return groupId;
+    }
+
+    public String generateGroupId(String group) {
+        String groupId = prefix + DELIMETER + group;
+        log.info("generateGroupId groupId: {}", groupId);
+        return groupId;
+    }
+
+}
