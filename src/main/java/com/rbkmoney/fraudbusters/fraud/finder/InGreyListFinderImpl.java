900da9eb32211b50b9171bcb95ab57035e546a7e
Kostya <kennny007@yandex.ru>
2019-10-08 17:27:41 +0300

    Fix rule whitelist (#48)
    * Fix empty value in lists

--- src/main/java/com/rbkmoney/fraudbusters/fraud/finder/InGreyListFinderImpl.java
+++ src/main/java/com/rbkmoney/fraudbusters/fraud/finder/InGreyListFinderImpl.java
@@ -1,7 +1,6 @@
 package com.rbkmoney.fraudbusters.fraud.finder;
 
 import com.rbkmoney.damsel.wb_list.*;
-import com.rbkmoney.fraudbusters.constant.EventField;
 import com.rbkmoney.fraudbusters.exception.RuleFunctionException;
 import com.rbkmoney.fraudbusters.fraud.resolver.FieldResolver;
 import com.rbkmoney.fraudbusters.repository.EventRepository;
@@ -10,6 +9,7 @@ import com.rbkmoney.fraudo.constant.CheckedField;
 import com.rbkmoney.fraudo.finder.InListFinder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 
 import java.time.Instant;
 import java.util.List;
@@ -27,20 +27,22 @@ public class InGreyListFinderImpl implements InListFinder {
     @Override
     public Boolean findInList(String partyId, String shopId, CheckedField field, String value) {
         try {
-            Row row = createRow(partyId, shopId, field, value);
-            Result result = wbListServiceSrv.getRowInfo(row);
-            if (result.getRowInfo() != null && result.getRowInfo().isSetCountInfo()) {
-                RowInfo rowInfo = result.getRowInfo();
-                String startCountTime = rowInfo.getCountInfo().getStartCountTime();
-                String ttl = rowInfo.getCountInfo().getTimeToLive();
-                String resolveField = fieldResolver.resolve(field);
-                Long to = TimestampUtil.generateTimestampWithParse(ttl);
-                Long from = TimestampUtil.generateTimestampWithParse(startCountTime);
-                if (Instant.now().getEpochSecond() > to || from >= to) {
-                    return false;
+            if (!StringUtils.isEmpty(value)) {
+                Row row = createRow(partyId, shopId, field, value);
+                Result result = wbListServiceSrv.getRowInfo(row);
+                if (result.getRowInfo() != null && result.getRowInfo().isSetCountInfo()) {
+                    RowInfo rowInfo = result.getRowInfo();
+                    String startCountTime = rowInfo.getCountInfo().getStartCountTime();
+                    String ttl = rowInfo.getCountInfo().getTimeToLive();
+                    String resolveField = fieldResolver.resolve(field);
+                    Long to = TimestampUtil.generateTimestampWithParse(ttl);
+                    Long from = TimestampUtil.generateTimestampWithParse(startCountTime);
+                    if (Instant.now().getEpochSecond() > to || from >= to) {
+                        return false;
+                    }
+                    int currentCount = eventRepository.countOperationByField(resolveField, value, from, to);
+                    return currentCount + CURRENT_ONE <= rowInfo.getCountInfo().getCount();
                 }
-                int currentCount = eventRepository.countOperationByField(resolveField, value, from, to);
-                return currentCount + CURRENT_ONE <= rowInfo.getCountInfo().getCount();
             }
             return false;
         } catch (Exception e) {
@@ -59,10 +61,10 @@ public class InGreyListFinderImpl implements InListFinder {
     }
 
     @Override
-    public Boolean findInList(String partyId, String shopId, List<CheckedField> fields, List<String> value) {
+    public Boolean findInList(String partyId, String shopId, List<CheckedField> fields, List<String> values) {
         try {
             for (int i = 0; i < fields.size(); i++) {
-                if (findInList(partyId, shopId, fields.get(i), value.get(i))) {
+                if (!StringUtils.isEmpty(values.get(i)) && findInList(partyId, shopId, fields.get(i), values.get(i))) {
                     return true;
                 }
             }
--- src/main/java/com/rbkmoney/fraudbusters/fraud/finder/InListFinderImpl.java
+++ src/main/java/com/rbkmoney/fraudbusters/fraud/finder/InListFinderImpl.java
@@ -8,6 +8,7 @@ import com.rbkmoney.fraudo.constant.CheckedField;
 import com.rbkmoney.fraudo.finder.InListFinder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,8 +23,11 @@ public class InListFinderImpl implements InListFinder {
     @Override
     public Boolean findInList(String partyId, String shopId, CheckedField field, String value) {
         try {
-            Row row = createRow(partyId, shopId, field, value);
-            return wbListServiceSrv.isExist(row);
+            if (!StringUtils.isEmpty(value)) {
+                Row row = createRow(partyId, shopId, field, value);
+                return wbListServiceSrv.isExist(row);
+            }
+            return false;
         } catch (Exception e) {
             log.warn("InListFinderImpl error when findInList e: ", e);
             throw new RuleFunctionException(e);
@@ -40,12 +44,14 @@ public class InListFinderImpl implements InListFinder {
     }
 
     @Override
-    public Boolean findInList(String partyId, String shopId, List<CheckedField> fields, List<String> value) {
+    public Boolean findInList(String partyId, String shopId, List<CheckedField> fields, List<String> values) {
         try {
             List<Row> rows = new ArrayList<>();
             for (int i = 0; i < fields.size(); i++) {
-                Row row = createRow(partyId, shopId, fields.get(i), value.get(i));
-                rows.add(row);
+                if (!StringUtils.isEmpty(values.get(i))) {
+                    Row row = createRow(partyId, shopId, fields.get(i), values.get(i));
+                    rows.add(row);
+                }
             }
             return wbListServiceSrv.isAnyExist(rows);
         } catch (Exception e) {
