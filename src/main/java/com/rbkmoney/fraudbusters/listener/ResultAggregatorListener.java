156de0908bef8ed3d00afde5489c52714b2b08a9
k.struzhkin <k.struzhkin@rbkmoney.com>
2019-11-26 18:39:42 +0300

    Add batch flag

--- src/main/java/com/rbkmoney/fraudbusters/listener/ResultAggregatorListener.java
+++ src/main/java/com/rbkmoney/fraudbusters/listener/ResultAggregatorListener.java
@@ -6,6 +6,8 @@ import com.rbkmoney.fraudbusters.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -19,8 +21,9 @@ public class ResultAggregatorListener {
     private final FraudResultToEventConverter fraudResultToEventConverter;
 
     @KafkaListener(topics = "${kafka.topic.result}", containerFactory = "kafkaListenerContainerFactory")
-    public void listen(List<FraudResult> batch) {
-        log.info("ResultAggregatorListener listen result: {}", batch);
+    public void listen(List<FraudResult> batch, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) Integer partition,
+                       @Header(KafkaHeaders.OFFSET) Long offset) {
+        log.info("ResultAggregatorListener listen result size: {} partition: {} offset: {}", batch.size(), partition, offset);
         eventRepository.insertBatch(fraudResultToEventConverter.convertBatch(batch));
     }
 }
