fbeaa19e4b9a545e42fc9ce765499b1fb88bac20
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2020-06-08 15:04:03 +0300

    Fix

--- src/main/java/com/rbkmoney/fraudbusters/config/properties/DefaultTemplateProperties.java
+++ src/main/java/com/rbkmoney/fraudbusters/config/properties/DefaultTemplateProperties.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Getter
 @Setter
 @Component
-@ConfigurationProperties(prefix = "defaultTemplate")
+@ConfigurationProperties(prefix = "default.template")
 public class DefaultTemplateProperties {
     private boolean enable;
     private int countToCheckYears;
