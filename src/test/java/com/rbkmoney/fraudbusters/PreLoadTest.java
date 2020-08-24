eca6992709e56a3cd15ff32da8b0cd646257997e
k.struzhkin <k.struzhkin@MacBook-Pro-rbkadmin.local>
2020-08-24 13:38:23 +0300

    Add flags

--- src/test/java/com/rbkmoney/fraudbusters/PreLoadTest.java
+++ src/test/java/com/rbkmoney/fraudbusters/PreLoadTest.java
@@ -31,7 +31,8 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @Slf4j
 @RunWith(SpringRunner.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-@SpringBootTest(webEnvironment = RANDOM_PORT, classes = FraudBustersApplication.class, properties = "kafka.listen.result.concurrency=1")
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = FraudBustersApplication.class,
+        properties = {"kafka.listen.result.concurrency=1", "kafka.historical.listener.enable=true"})
 @ContextConfiguration(initializers = KafkaAbstractTest.Initializer.class)
 public class PreLoadTest extends KafkaAbstractTest {
 
