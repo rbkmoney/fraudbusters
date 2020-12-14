b97c41ab936198192f7a1d3928e5128daad4710a
k.struzhkin <koztyastruga@gmail.com>
2020-12-14 18:54:33 +0300

    Fix topic

--- src/main/java/com/rbkmoney/fraudbusters/service/InitiatingEntitySourceService.java
+++ src/main/java/com/rbkmoney/fraudbusters/service/InitiatingEntitySourceService.java
@@ -3,6 +3,7 @@ package com.rbkmoney.fraudbusters.service;
 import com.rbkmoney.damsel.fraudbusters.ReferenceInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +12,18 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class InitiatingEntitySourceService {
 
+    @Value("${kafka.topic.unknown-initiating-entity}")
+    private String topic;
+
     private final KafkaTemplate<String, ReferenceInfo> kafkaUnknownInitiatingEntityTemplate;
 
     public void sendToSource(ReferenceInfo referenceInfo) {
         try {
             log.info("Got a new entity ('{}'). We should try to create a default template.", referenceInfo);
-            kafkaUnknownInitiatingEntityTemplate.send("", referenceInfo);
+            kafkaUnknownInitiatingEntityTemplate.send(topic, referenceInfo);
             log.info("New entity with successfully send to source entity ('{}')", referenceInfo);
         } catch (Exception e) {
-            log.error("Could not interact with fraudbusters-management", e);
+            log.error("Could send referenceInfo: {} to topic: {}", referenceInfo, topic, e);
         }
     }
 }
