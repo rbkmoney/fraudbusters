ff517a97f41df887c40959d8740534300efef3f2
k.struzhkin <koztyastruga@gmail.com>
2021-02-15 12:06:12 +0300

    Fix config

--- src/main/java/com/rbkmoney/fraudbusters/schedule/CardTokenPoolManagedJob.java
+++ src/main/java/com/rbkmoney/fraudbusters/schedule/CardTokenPoolManagedJob.java
@@ -3,12 +3,14 @@ package com.rbkmoney.fraudbusters.schedule;
 import com.rbkmoney.fraudbusters.service.CardPoolManagementService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@ConditionalOnProperty(value="load.isTrusted.enabled")
 public class CardTokenPoolManagedJob {
 
     private final CardPoolManagementService cardPoolManagementService;
