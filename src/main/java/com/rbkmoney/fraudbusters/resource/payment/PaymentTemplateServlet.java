ca05dee36dbdc7885bd19d680731e4c0529b1cc2
k.struzhkin <koztyastruga@gmail.com>
2021-07-26 17:25:19 +0300

    Add servlet for historical data

---
+++ src/main/java/com/rbkmoney/fraudbusters/resource/payment/PaymentTemplateServlet.java
@@ -0,0 +1,30 @@
+package com.rbkmoney.fraudbusters.resource.payment;
+
+import com.rbkmoney.damsel.fraudbusters.PaymentServiceSrv;
+import com.rbkmoney.woody.thrift.impl.http.THServiceBuilder;
+import lombok.RequiredArgsConstructor;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+
+@WebServlet("/fraud_payment_validator/v1/")
+@RequiredArgsConstructor
+public class PaymentTemplateServlet extends GenericServlet {
+
+    private final PaymentServiceSrv.Iface paymentHandler;
+    private Servlet thriftServlet;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        thriftServlet = new THServiceBuilder()
+                .build(PaymentServiceSrv.Iface.class, paymentHandler);
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        thriftServlet.service(req, res);
+    }
+}
